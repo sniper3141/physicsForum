dd4890aa1a127418ab691534d8c3e6a12027fcb6
Hartman <hartmaj@esms.org.uk>
2022-11-25 10:59:54 +0000

    More styling to the forum page

--- mainForum.php
+++ mainForum.php
@@ -51,7 +51,7 @@
                 echo '
                     <div class="forumItem">
                         <div class="emailAndTimeContainer"> 
-                            <h5 class="userEmail">'.$questionInfo[$row*3 + 2].'</h5>
+                            <h6 class="userEmail">'.$questionInfo[$row*3 + 2].'</h5>
                             <h6 class="timeOfQuestion">'.$questionInfo[$row*3 + 1].'</h6>
                         </div>
                         <p>'.$questionInfo[$row*3].'</p>
@@ -69,8 +69,8 @@
         <div id="textWrapper" onclick="clicked()">
             <h2 id="newQuestionButton">New Question</h2>
         </div>
-        <form class='form signup' id="hidden" action="./includes/handleForum.inc.php" method="post" autocomplete="off">
-            <input type="text" name="mainForum" required>
+        <form class='form' action="./includes/handleForum.inc.php" method="post" autocomplete="off">
+            <input class="forumInput" id="noWidth" type="text" name="mainForum" placeholder="Enter a Question" required>
         </form>
     </div>
 </body>
@@ -91,14 +91,25 @@
         }, 100)
         setTimeout(() => {
             button.style.display = "none";
+            const forum = document.querySelector(".forumInput");
+            forum.style.display = "flex";
+            forum.style.width = "40vw";
         }, 300)
         
-        // console.log("1")
     }
 
     function showButton(){
         const button = document.querySelector("#textWrapper");
         const wrapper = document.querySelector("#formWrapper")
+        const forum = document.querySelector(".forumInput");
+
+        if (forum === document.activeElement){
+            // console.log("true")
+            return;
+        }
+
+        forum.style.display = "none";
+        forum.style.width = "0";
         if (button.style.display != "none"){
             setTimeout(() => {
                 button.style.display = "flex";
@@ -117,7 +128,10 @@
             button.style.color = "rgb(255, 255, 255, 1)";
             wrapper.removeEventListener("click", showButton);
         }, 100)
-        console.log("2")
     }
+
+    // showInput(){
+        
+    // }
 </script>
 </html>
